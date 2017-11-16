65b4d7eb2f718d9c58f2b137f7866acbdab38536
Margauxlax <33702024+Margauxlax@users.noreply.github.com>
2017-11-16 13:27:42 +0100

    Add files via upload

---
+++ Caesar.rb
@@ -0,0 +1,16 @@
+def caesar_cipher(string, shift = 5)
+  alphabet  = Array('a'..'z')
+  non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  alphabet = Array('A'..'Z')
+  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+
+  alphabet = Array('!'..'?')
+  symbol     = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  encrypter = symbol.merge(non_caps.merge(caps))
+  
+  string.chars.map { |c| encrypter.fetch(c, c) }
+end
+
+p caesar_cipher("What a string!").join
